236ebe72af8c3a5f19260f21df786ba0cc870b16
Otis <otis.nguyen.goldenowl@gmail.com>
2021-09-20 13:35:44 +0700

    chore: add path alias

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const HelloWorld = () => (
+  <View style={styles.container}>
+    <Text>hello hello!</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default HelloWorld;
